e7bdb24b1e37da5dad0e350a968b149d8d5f843e
petcha <petcha@pop-os.localdomain>
2024-01-12 15:15:57 -0300

    Feat: added require easy manager

--- src/Managers/RequireManager.php
+++ src/Managers/RequireManager.php
@@ -10,21 +10,19 @@ class RequireManager
     /**
      * Require all route files in the 'easy' directory.
      *
-     * @param string $type The type of routes to include ('web' or 'api').
      * @throws Exception
      */
-    public static function requireEasyRoutes(string $type): void
+    public static function requireEasyRoutes(): void
     {
         $directoryPath = base_path('routes/easy');
+
         if (!File::isDirectory($directoryPath) || count(File::files($directoryPath)) === 0) {
             throw new Exception("The 'easy' route directory does not exist or is empty.");
         }
+
         $routeFiles = File::allFiles($directoryPath);
         foreach ($routeFiles as $file) {
-            $filePath = $file->getPathname();
-            if (str_contains($filePath, $type)) {
-                require $filePath;
-            }
+            require $file->getPathname();
         }
     }
 }
